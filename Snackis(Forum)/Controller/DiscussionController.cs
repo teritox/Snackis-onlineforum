b84cee8a7e99ab9821ebef423d4abfec320835de
Carro-J <71273876+Carro-J@users.noreply.github.com>
2021-05-31 14:02:33 +0200

    Add feature to get forumposts through API with thread ID

---
+++ Snackis(Forum)/Controller/DiscussionController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Snackis_Forum_.Models;
+using Snackis_Forum_.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Snackis_Forum_.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscussionController : ControllerBase
+    {
+        private readonly IForumDataService _ds;
+        public DiscussionController(IForumDataService ds)
+        {
+            _ds = ds;
+        }
+
+        //GET api/<DiscussionController>/5
+        [HttpGet("{id}")]
+
+        public async Task<IEnumerable<Post>> GetAsync(int id)
+        {
+            var posts = await _ds.GetForumPosts(id);
+
+            if(posts == null)
+            {
+                return (IEnumerable<Post>)NotFound();
+            }
+
+            return posts;
+        }
+    }
+}
