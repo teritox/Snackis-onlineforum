dc8bb8786b1a5d6a7267541fc4a1356bae2c096f
Carro-J <71273876+Carro-J@users.noreply.github.com>
2021-06-02 13:26:08 +0200

    Add feature for replying to private messages with prefilled form for titel "Re: " and receiver name.

--- Snackis(Forum)/Pages/NewMessage.cshtml.cs
+++ Snackis(Forum)/Pages/NewMessage.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
+using System.Security.Policy;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -20,13 +21,23 @@ namespace Snackis_Forum_.Pages
         public UserManager<ForumUser> _userManager;
 
         [BindProperty]
-        public string Title { get; set; }
+        public string Title { get; set; } = "";
 
         [BindProperty]
-        public string TextMessage { get; set; }
+        public string TextMessage { get; set; } = "";
 
         [BindProperty]
-        public string Receiver { get; set; }
+        public string Receiver { get; set; } = "";
+
+
+        [BindProperty(SupportsGet = true)]
+        public int MessageID { get; set; }
+
+        public bool ReceiverFound { get; set; } = true;
+
+        public bool MessageSent { get; set; } = false;
+
+        public bool MessageFound { get; set; } = true;
 
 
         public NewMessageModel(ForumContext ctx, UserManager<ForumUser> userManager)
@@ -34,16 +45,35 @@ namespace Snackis_Forum_.Pages
             _ctx = ctx;
             _userManager = userManager;
         }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            if (MessageID != 0)
+            {
+                var message = await _ctx.PrivateMessages.FindAsync(MessageID);
+                Title = "Re: " + message.MessageTitle;
+
+                var sender = _userManager.FindByIdAsync(message.SenderId).Result;
+                Receiver = sender.NickName;
+            }
+
+            return Page();
+        }
         public async Task<IActionResult> OnPostAsync()
         {
 
-            if (Title != null & TextMessage != null)
+            if (TextMessage != null)
             {
-                var receiver = _userManager.Users.Where(u => u.NickName.ToLower() == Receiver.ToLower()).FirstOrDefault();
-                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-
-                if (receiver != null)
+                if (Receiver != null)
                 {
+                    if (Title == "" || String.IsNullOrWhiteSpace(Title))
+                    {
+                        Title = "(Inget ämne)";
+                    }
+
+                    var receiver = _userManager.Users.Where(u => u.NickName.ToLower() == Receiver.ToLower()).FirstOrDefault();
+                    var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
                     PrivateMessage message = new PrivateMessage
                     {
                         MessageTitle = Title,
@@ -57,8 +87,22 @@ namespace Snackis_Forum_.Pages
 
                     await _ctx.SaveChangesAsync();
 
+                    Title = "";
+                    Receiver = "";
+                    TextMessage = "";
+
+                    MessageSent = true;
+
                     return Page();
                 }
+                else
+                {
+                    ReceiverFound = false;
+                }
+            }
+            else
+            {
+                MessageFound = false;
             }
 
             return Page();
